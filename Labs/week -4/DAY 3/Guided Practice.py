862aaee3975bc7f656e50eeeb7013286b526ebdf
Taif F <taif7falibrahim@gmail.com>
2026-08-25 14:40:44 +0300

    Updating

--- Labs/week -4/DAY 3/Guided Practice.py
+++ Labs/week -4/DAY 3/Guided Practice.py
@@ -1,4 +1,4 @@
-from pathlip import Path           #استورد path عشان اتعامل مع مسارات المجلدات والملفات
+from pathlib import Path           #استورد path عشان اتعامل مع مسارات المجلدات والملفات
 import json               #استورد json لقراءة وكتابة بيانات json
 
 
@@ -8,11 +8,11 @@ data_dir.mkdir(exist_ok=True)           #انشئ المجلد اذا ماهو �
 file_path = data_dir / "students.json"         #احدد اسم ومسار الملف
 
 students = [
-    {"name": "طيف", "score": 99},            #سويت comprehinsion للطلاب
+    {"name": "طيف", "score": 99},            #سويت Dictionary للطلاب
     {"name": "Mashael", "score": 77}
 ]
 
-with open(file_path, "w", encoding"utf-8") as file:        #افتح الملف للكتابة واحط انكودنق utf8 عشان اذا كتبت عربي
+with open(file_path, "w", encoding="utf-8") as file:        #افتح الملف للكتابة واحط انكودنق utf8 عشان اذا كتبت عربي
     json.dump(students, file, indent = 4)        #حول بيانات بايثون لjson
 
 
@@ -26,14 +26,14 @@ def validate_student(student):                                 #ابغى اتح�
         raise InvaildStudentsError("student name is rquired")
     if "score" not in student:                                     #أستخدم if عشان اتحقق من كل الشروط اللي أبغاها
         raise InvaildStudentsError("student score is reqired")
-    if not isinstance(student["score"]), (int, float):             #أتأكد ان الدرجة ماهي نص بل رقم او فلوت
+    if not isinstance(student["score"], (int, float)):             #أتأكد ان الدرجة ماهي نص بل رقم او فلوت
         raise InvaildStudentsError("score must be a number")
-    if not 0 >= (student["score"]) <= 100:
+    if not 0 <= (student["score"]) <= 100:
         raise InvaildStudentsError("score must be between 0 and 100 ")
 
 
 try:                      #جرب تفتح الملف و تقرأه
-    with open(file_path, "r", encoding"utf-8") as file: 
+    with open(file_path, "r", encoding="utf-8") as file: 
         students = json.load(file)                    #حمل بيانات json و حولها ل python
 
     for student in students:
@@ -41,10 +41,11 @@ try:                      #جرب تفتح الملف و تقرأه
 
 except FileNotFoundError:                                     #ابغى اعلمه كيف يتعامل بكل حالة تصير معاه
     print("Students file not found")            #في حالة الملف مو موجود
-except json.JSONDecodeError:
+except json.JSONDecodeError:                              #في حالة الملف غير صالح
     print("invaild json file")
 
-if not student["name"]:
-if not 0 <= students["score"] <= 100:
 
-raise ValueError ("Score must be between 0 and 100")
+else:                                      #يشتغل فقط اذا تمت العملية بدون أخطاء
+    print("Students loaded successfully")
+finally:                                                        #يشتغل دايم سواء تمت العملية أم لا
+    print("Load attempt finished")
