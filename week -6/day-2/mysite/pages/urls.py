df207d4c20d38072c9eeb6a3b0c3f17193cfdaa3
Taif Alosaimi <taif7falibrahim@gmail.com>
2026-09-02 19:37:48 +0300

    Delete week -6/day-2 directory

--- week -6/day-2/mysite/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, faq, team
-
-urlpatterns = [
-    path('', index, name="index"),
-    path("faq/", faq, name="faq"),
-    path("team/", team, name="team"),
-]
