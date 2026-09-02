cd0633e7e10b90f84c4073e6bc8a3621891f8376
Taif F <taif7falibrahim@gmail.com>
2026-09-02 19:31:10 +0300

    Add Day 3 Lab & Challenge

--- movies/mysite/movie/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path("", views.movie_list, name="movie_list"),
-    path("<int:id>/", views.movie_detail, name="movie_detail"),
-]
-
-
