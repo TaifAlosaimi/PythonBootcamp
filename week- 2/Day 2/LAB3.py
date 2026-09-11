3195401a125405085a9d3c386c275d47f58a1d41
Taif F <taif7falibrahim@gmail.com>
2026-09-11 23:42:51 +0300

    Just improving code

--- week- 2/Day 2/LAB3.py
+++ week- 2/Day 2/LAB3.py
@@ -1,14 +1,14 @@
 student_name, student_age, student_is_registered = "Saleh", 24, True
 
 print(type(student_age))
-print(type(student_name))
+print(type(student_name))               #using type() so yk the data type
 print(type(student_is_registered))
 
-print(isinstance(student_age, int))
+print(isinstance(student_age, int))            #using isinstance() for data type checking
 
-age = input("Enter your age: ")
+age = input("Enter your age: ")                  #input is always STRING!
 
-if isinstance(age, int):
+if isinstance(age, int):                    #If used لتنفيذ كود عند  تحقق الشرط
     print("You are", age + 5, "after 5 years")
 else:
     print("You are", int(age) + 5, "after 5 years")
@@ -19,7 +19,7 @@ print(teacher_name)
 
 index = int(input("Select an index: "))
 
-if index < len(teacher_name):
+if index < len(teacher_name):                       #len used لعدد العناصر
     print(teacher_name[index])
 else:
     print("Out of range")
